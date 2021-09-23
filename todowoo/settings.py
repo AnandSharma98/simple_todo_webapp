781c47834e4a7e132af54780b76e1b4ba488310f
anand <anand00525@gmail.com>
2021-09-23 19:28:56 +0530

    background changes

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c2^b%t!n%6&q9%133ne3lc5#1$=3d8j5$u+u!+sve4pxc27fu2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todowoo0.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
